1c32e44563bf223bb3c3577796b70765f00e2c38
Vikas Ranjan <vikashraz007@gmail.com>
2023-02-09 21:53:29 +0530

    E1

---
+++ client/pages/Index.jsx
@@ -0,0 +1,151 @@
+import { useAppBridge } from "@shopify/app-bridge-react";
+import { Redirect } from "@shopify/app-bridge/actions";
+import { Card, Layout, Page } from "@shopify/polaris";
+import { navigate } from "raviger";
+import React from "react";
+
+const HomePage = () => {
+  const app = useAppBridge();
+  const redirect = Redirect.create(app);
+
+  return (
+    <Page title="Home">
+      <Layout>
+        <Layout.Section fullWidth>
+          <Card
+            title="Debug Cards"
+            sectioned
+            primaryFooterAction={{
+              content: "Debug Cards",
+              onAction: () => {
+                navigate("/debug");
+              },
+            }}
+          >
+            <p>
+              Explore how the repository handles data fetching from the backend,
+              App Proxy, making GraphQL requests, Billing API and more.
+            </p>
+          </Card>
+        </Layout.Section>
+        <Layout.Section oneHalf>
+          <Card
+            sectioned
+            title="Repository"
+            primaryFooterAction={{
+              content: "GitHub",
+              onAction: () => {
+                redirect.dispatch(Redirect.Action.REMOTE, {
+                  url: "https://github.com/kinngh/shopify-node-express-mongodb-app",
+                  newContext: true,
+                });
+              },
+            }}
+            secondaryFooterActions={[
+              {
+                content: "Open Issue",
+                onAction: () => {
+                  redirect.dispatch(Redirect.Action.REMOTE, {
+                    url: "https://github.com/kinngh/shopify-node-express-mongodb-app/issues?q=is%3Aissue",
+                    newContext: true,
+                  });
+                },
+              },
+            ]}
+          >
+            <p>Star the repository, open a new issue or start a discussion.</p>
+          </Card>
+          <Card
+            sectioned
+            title="Changelog"
+            primaryFooterAction={{
+              content: "Explore",
+              onAction: () => {
+                redirect.dispatch(Redirect.Action.REMOTE, {
+                  url: "https://shopify.dev/changelog/",
+                  newContext: true,
+                });
+              },
+            }}
+          >
+            <p>Explore changelog on Shopify.dev and follow updates.</p>
+          </Card>
+        </Layout.Section>
+        <Layout.Section oneHalf>
+          <Card
+            sectioned
+            title="Documentation"
+            primaryFooterAction={{
+              content: "Explore APIs",
+              onAction: () => {
+                redirect.dispatch(Redirect.Action.REMOTE, {
+                  url: "https://shopify.dev/graphiql/admin-graphiql",
+                  newContext: true,
+                });
+              },
+            }}
+            secondaryFooterActions={[
+              {
+                content: "Design Guidelines",
+                onAction: () => {
+                  redirect.dispatch(Redirect.Action.REMOTE, {
+                    url: "https://shopify.dev/apps/design-guidelines",
+                    newContext: true,
+                  });
+                },
+              },
+            ]}
+          >
+            <p>
+              Explore the GraphQL APIs in Graphiql or read design guidelines.
+            </p>
+          </Card>
+          <Card
+            sectioned
+            title="Hiring?"
+            primaryFooterAction={{
+              content: "Twitter",
+              onAction: () => {
+                redirect.dispatch(Redirect.Action.REMOTE, {
+                  url: "https://www.twitter.com/kinngh",
+                  newContext: true,
+                });
+              },
+            }}
+            secondaryFooterActions={[
+              {
+                content: "LinkedIn",
+                onAction: () => {
+                  redirect.dispatch(Redirect.Action.REMOTE, {
+                    url: "https://www.linkedin.com/in/theharshdeep/",
+                    newContext: true,
+                  });
+                },
+              },
+            ]}
+          >
+            <p>🌎 / 🇨🇦 and looking to expand your engineering team?</p>
+          </Card>
+        </Layout.Section>
+        <Layout.Section fullWidth>
+          <Card
+            sectioned
+            title="Developer Notes"
+            primaryFooterAction={{
+              content: "Read More",
+              onAction: () => {
+                navigate("/debug/devNotes");
+              },
+            }}
+          >
+            <p>
+              Read notes on opening an issue, creating App Extensions and more.
+            </p>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+};
+
+export default HomePage;
